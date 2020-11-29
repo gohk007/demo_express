cb4fc4b7077ac63bcbbfcfa4ab0d946da6690cf5
Truly Mittal <trulymittal@gmail.com>
2020-11-30 02:17:44 +0530

    added express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('It works...');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`🚀 on port ${process.env.PORT}`);
+});
