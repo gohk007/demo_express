d35a72321d97a9755644bea2c3bbd8093eeb76f4
Hemanth <gohk007@gmail.com>
2021-01-02 14:05:15 +0530

    product listings added

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('It works...updated now from developer');
 });
 
+
+
+app.get('/Products', (req, res) => {
+res.send('Products listing');	
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`🚀 on port ${process.env.PORT || 3000}`);
 });
