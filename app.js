9b21225fc07d17b2f7ee83fa84ffaa81e13886f2
gohk007 <gohk007@gmail.com>
2021-03-17 16:44:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('It works...updated now from developer');
+  res.send('It works...updated now from developer hai from github');
 });
 
 
