b95cf74bf54a6e48d38b23ee6f46163ba4073ef7
hemanthgowdaa <47692827+hemanthgowdaa@users.noreply.github.com>
2021-03-17 17:25:26 +0530

    login directory

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('It works...updated now from developer hai from github');
+app.get("/", (req, res) => {
+  res.send("It works...updated now from developer hai from github");
 });
 
+app.get("/product", (req, res) => {
+  res.send("Product page");
+});
 
-
-app.get('/Products', (req, res) => {
-res.send('Products listing');	
+app.get("/login", (req, res) => {
+  res.sendFile("views/index.html", { root: __dirname });
 });
 
 app.listen(process.env.PORT || 3000, () => {
